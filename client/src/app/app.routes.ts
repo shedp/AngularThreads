2e67f4cc346b611de5905c8eeb5df5898411f2d2
shedp <sheenadiep.gmail.com>
2024-04-17 16:01:39 +0100

    adding a global navbar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,4 +6,9 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('./user/user.component').then((m) => m.UserComponent),
+  },
 ];
