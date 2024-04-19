1d797aa76a654ea520abd2e9f8b4c2ae1399ae82
shedp <sheenadiep.gmail.com>
2024-04-19 16:13:35 +0100

    ui for reply form

---
+++ client/src/app/components/reply-form/reply-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './reply-form.component.html',
+  styleUrl: './reply-form.component.css',
+})
+export class ReplyFormComponent {}
