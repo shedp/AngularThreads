61b1ca299845d6c0c152030f8a60c595fc92bee7
shedp <sheenadiep.gmail.com>
2024-04-19 16:14:11 +0100

    ui for comment options

---
+++ client/src/app/components/comment-options/comment-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './comment-options.component.html',
+  styleUrl: './comment-options.component.css'
+})
+export class CommentOptionsComponent {
+
+}
--- client/src/app/components/comment/comment.component.ts
+++ client/src/app/components/comment/comment.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommentOptionsComponent } from '../comment-options/comment-options.component';
+import { ReplyFormComponent } from '../reply-form/reply-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [CommentOptionsComponent, ReplyFormComponent, CommonModule],
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.css'
+  styleUrl: './comment.component.css',
 })
 export class CommentComponent {
+  isReplyExpanded = false;
+  commentsIsMore = false;
+  comments = ['hereisacoments', 'here is another ocmments', 'a thrid'];
 
+  toggleReplyExpanded = () => {
+    this.isReplyExpanded = !this.isReplyExpanded;
+  };
+
+  showMoreComments = () => {
+    if (this.comments.length > 3) {
+      this.commentsIsMore = true;
+    }
+  };
 }
